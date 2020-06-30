fa69a8d7da7c8f7745e5e45dfa75b42a9fc86da6
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-06-30 15:54:21 -0300

    create class variables.

---
+++ scripts/variables.js
@@ -0,0 +1,33 @@
+let imageScenario;
+let imageCharacter;
+let imageCharacterEnemy;
+let imageCharacterEnemyTroll;
+let imageGameOver;
+let imageIniitalScene;
+let imageLife;
+
+let scenario;
+let level01;
+let currentlyScene = 'initialScene';
+let scenes;
+let initialScene;
+let managerButton;
+
+let initialFont;
+let life;
+let tape;
+
+let character;
+let characterEnemy;
+let characterEnemyTroll;
+
+let soundGame;
+let jumpSoundCharacter;
+let soundGameOver;
+let soundColision;
+
+let points;
+let currentlyEnemy = 0;
+
+const enemys = [];
+
